0d647da49a896ba378a0f79df1b40ed4fe786f0c
artemPGH <facts.com99@gmail.com>
2026-03-11 21:15:20 +0100

    Add news data for Minecraft updates and mods

---
+++ news-data.js
@@ -0,0 +1,14 @@
+const newsList = [
+    {
+        title: "Вышло обновление Minecraft 1.21.1",
+        description: "В этом патче разработчики исправили критические баги с редстоуном и обновили текстуры некоторых блоков. Рекомендуем обновиться!",
+        image: "https://www.minecraft.net/content/dam/games/minecraft/key-art/TrickyTrials_Standard_Vertical.jpg",
+        date: "11.03.2026"
+    },
+    {
+        title: "Топ модов для выживания",
+        description: "Мы собрали 10 лучших модов, которые сделают ваше выживание в 2026 году по-настоящему хардкорным.",
+        image: "https://img.youtube.com/vi/jSOfnZ-P3E4/maxresdefault.jpg",
+        date: "10.03.2026"
+    }
+];
